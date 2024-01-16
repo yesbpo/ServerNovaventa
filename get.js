e51a14a22a5df296e351ae413ca835248fd062e1
unknown <philiprockert@gmail.com>
2024-01-16 14:08:18 -0500

    first commit

--- get.js
+++ get.js
@@ -223,8 +223,6 @@ const segundos = fechaActual.toLocaleString('en-US', { second: '2-digit', timeZo
          
       }}
       //chats no creados
-      
-
       const datamensaje = await response.json();
       // Manejar la respuesta según tus necesidades
     } catch (error) {
@@ -256,38 +254,7 @@ const segundos = fechaActual.toLocaleString('en-US', { second: '2-digit', timeZo
 
       
       }
-     // const responseChat = await fetch('https://appcenteryes.appcenteryes.com/db/obtener-chats');
-     // const chats = await responseChat.json();
-     // const chatlimpio = chats.filter(chat=> chat.idChat2 == data.payload.source);
-     // const chatlimpio1 = chatlimpio.filter(chat=> chat.status == 'closed')
-      
-     // if(chatlimpio[0].status == 'closed'&& data.type == 'message'){
-      //  console.log('entra en if')
-        
-      //  console.log('log de obtener',chatlimpio)
-        
-       //   console.log('entra en if2')
-        //  const data1 = {
-            
-        //   idChat2: chatlimpio[0].idChat2,
-         //  resolved: false,
-          // status: 'pending',
-           //userId: 0,
-         //};
-         //const response = await fetch('https://appcenteryes.appcenteryes.com/db/crear-chat', {
-          // method: 'POST',
-          // headers: {
-            // 'Content-Type': 'application/json',
-           //},
-           //body: JSON.stringify(data1),
-         //});  
-         //if (!response.ok) {
-          // console.log('no exito')       
-        // }
-        // const responseData = await response.json();
-         // console.log(responseData)
-         
-      //}
+     
       
      } catch (error) {
       // Maneja cualquier error durante el procesamiento asíncrono
@@ -302,7 +269,7 @@ const segundos = fechaActual.toLocaleString('en-US', { second: '2-digit', timeZo
      const fechaActual = new Date();
 const options = { timeZone: 'America/Bogota', hour12: false };
      const fechaInicio = new Date(fechaActual);
-     fechaInicio.setMinutes(fechaInicio.getMinutes() - 30);
+     fechaInicio.setMinutes(fechaInicio.getMinutes() - 5);
      
      // Formatear la fecha de inicio
      const anioInicio = fechaInicio.toLocaleString('en-US', { year: 'numeric', timeZone: options.timeZone });
