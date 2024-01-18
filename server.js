4abf0f8baefafae09f4e7805f2f706a0b41ca1e0
unknown <philiprockert@gmail.com>
2024-01-18 11:59:17 -0500

    first commit

--- server.js
+++ server.js
@@ -215,7 +215,7 @@ app.post(process.env.DB_ROUTE+'/guardar-mensajes', async (req, res) => {
       // Si ya existe, actualiza los demás datos
       const [updateResult] = await promisePool.execute(
         'UPDATE Mensaje SET type_comunication = ?, status = ?, number = ?, type_message = ?, content = COALESCE(?, content) WHERE idMessage = ?',
-        [type_comunication, status, number, type_message, idMessage]
+        [type_comunication, status, number, type_message, content, idMessage]
       );
 
       if (updateResult.affectedRows > 0) {
