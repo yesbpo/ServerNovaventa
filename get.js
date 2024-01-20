83cc6d21ace201016fffe8a355b264e7bef07e36
unknown <philiprockert@gmail.com>
2024-01-20 12:30:10 -0500

    first commit

--- get.js
+++ get.js
@@ -171,7 +171,7 @@ const options = { timeZone: 'America/Bogota', hour12: false };
         
         
         const nameuser = usuariosC.find(user => user.id === chats[0].userId).complete_name
-        console.log(usuariosC)       
+             
         const conver = {
           idchat: chats[0].idChat2,
           asesor: nameuser,
