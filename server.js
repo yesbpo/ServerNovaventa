13e4139dd26af14bdb9c737cf60285d2e01a45e3
unknown <philiprockert@gmail.com>
2024-01-24 13:17:02 -0500

    first commit

--- server.js
+++ server.js
@@ -505,7 +505,7 @@ app.post(process.env.DB_ROUTE + '/insertar-conversacion', async (req, res) => {
       const existingConversation = existingConversations[existingConversations.length-1];
 
       if (existingConversation.userid !== userid) {
-        console.log('ingresa', existingConversation)
+
         // Crear una nueva instancia ya que el userid es diferente al existente
         await promisePool.execute(
           'INSERT INTO Conversation (idchat, asesor, conversacion, numero, calificacion, fecha_ingreso, fecha_ultimagestion, userid) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
