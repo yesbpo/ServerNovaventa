1e79af52f90866b416e1fb303661abd0c5432999
unknown <philiprockert@gmail.com>
2024-02-12 09:46:38 -0500

    select para consultar chats por rango de fechas

--- server.js
+++ server.js
@@ -513,7 +513,7 @@ app.get(process.env.DB_ROUTE + '/consultar-chats-semana', async (req, res) => {
 
     // Establecer la fecha de inicio de hoy a las 00:00:00
     const startOfDay = new Date();
-    startOfDay.setDate(startOfDay.getDate()-7);
+    startOfDay.setDate(startOfDay.getDate() - startOfDay.getDay() + (startOfDay.getDay() === 0 ? -6 : 1));
 
     // Establecer la fecha de finalización de hoy a las 23:59:59
     const endOfDay = new Date();
